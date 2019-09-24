e43a5bb7eb6fd4159d86b0f64a2f936bc44675a1
Raman Gupta <ramanxg@gmail.com>
2019-09-24 14:37:38 -0700

    Updates

---
+++ precache-manifest.aff2a41833c000d5b594c6c4999250e8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eda1334f8ed76291d4e29660afcbcf7",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "2fab93e34253af0cab76",
+    "url": "/Portfolio/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "9d2377e5e66f90ac8e89",
+    "url": "/Portfolio/static/css/main.21b51dbb.chunk.css"
+  },
+  {
+    "revision": "2fab93e34253af0cab76",
+    "url": "/Portfolio/static/js/2.c1e04951.chunk.js"
+  },
+  {
+    "revision": "9d2377e5e66f90ac8e89",
+    "url": "/Portfolio/static/js/main.4e90b615.chunk.js"
+  },
+  {
+    "revision": "dd4ccac87fb3e093eacc",
+    "url": "/Portfolio/static/js/runtime-main.665a1c84.js"
+  },
+  {
+    "revision": "b23f070405893ef33305ce943ea2ea5b",
+    "url": "/Portfolio/static/media/Resume.b23f0704.docx"
+  },
+  {
+    "revision": "6349649e3f7e3ca32783c3543012fccd",
+    "url": "/Portfolio/static/media/convnet.6349649e.png"
+  },
+  {
+    "revision": "d150d4d60b034a7b8fc0df36dd389590",
+    "url": "/Portfolio/static/media/github-circle.d150d4d6.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/Portfolio/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "8fcac3b248cff064c0d887b34489cd24",
+    "url": "/Portfolio/static/media/linkedin.8fcac3b2.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "0158e84d1e90972c23907f1d28c74c00",
+    "url": "/Portfolio/static/media/neuralnet.0158e84d.png"
+  },
+  {
+    "revision": "2924210a438675246690ce91af01d564",
+    "url": "/Portfolio/static/media/profpic.2924210a.jpg"
+  },
+  {
+    "revision": "fc53d589c639f07699d8c8bfebce3608",
+    "url": "/Portfolio/static/media/reco.fc53d589.png"
+  },
+  {
+    "revision": "2376a41a0c6bdf724332f7a0a6575693",
+    "url": "/Portfolio/static/media/twitter.2376a41a.jpg"
+  },
+  {
+    "revision": "0a8d8b0d62911f2321485dc851a0961b",
+    "url": "/Portfolio/static/media/website.0a8d8b0d.png"
+  }
+]);
